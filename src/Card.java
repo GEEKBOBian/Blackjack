37e1c7b9a40528416245e6f9f53642cbd258ee19
GEEKBOBian <ian_smith26@milton.edu>
2025-05-16 13:50:39 -0400

    start of project

---
+++ src/Card.java
@@ -0,0 +1,9 @@
+public class Card {
+
+    public String number; //ex: 1-K
+    public String suit; //ex: club, spade, heart, diamond
+    public boolean isEleven; //this is for aces
+    public int value; //ex: 1, 2, 10, 15, 21
+
+
+}
